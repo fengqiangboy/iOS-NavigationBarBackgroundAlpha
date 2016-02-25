a3ae65c62b374a66f8a73c21775b94d4d9a93e39
FengQiang <498460828@qq.com>
2016-02-25 15:46:54 +0800

    添加方案1

---
+++ 1-SetNavigationBarAlpha/1-SetNavigationBarAlpha/ViewController.m
@@ -0,0 +1,50 @@
+//
+//  ViewController.m
+//  1-SetNavigationBarAlpha
+//
+//  Created by 奉强 on 16/2/25.
+//  Copyright © 2016年 奉强. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#define WIDTH [UIScreen mainScreen].bounds.size.width
+#define HEIGHT [UIScreen mainScreen].bounds.size.height
+
+@interface ViewController () <UIScrollViewDelegate>
+
+@property (nonatomic, strong) UIScrollView *scrollView;
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self setScrollView];
+}
+
+- (void)setScrollView {
+    self.scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    UIImage *image = [UIImage imageNamed:@"111.png"];
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, WIDTH, (image.size.width/WIDTH)*HEIGHT)];
+    self.scrollView.contentSize = CGSizeMake(0, imageView.bounds.size.height);
+    self.scrollView.delegate = self;
+    imageView.image = image;
+    
+    [self.scrollView addSubview:imageView];
+    
+    
+    [self.view addSubview:self.scrollView];
+}
+
+#pragma mark scrollViewDelegate
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    self.navigationController.navigationBar.alpha = scrollView.contentOffset.y / 200.0f;
+}
+
+
+
+@end
---
+++ 1-SetNavigationBarAlpha/1-SetNavigationBarAlpha/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  1-SetNavigationBarAlpha
+//
+//  Created by 奉强 on 16/2/25.
+//  Copyright © 2016年 奉强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
