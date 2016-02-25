597184dac9a6558b3255a1bb617ccf9ae98ff8fe
FengQiang <498460828@qq.com>
2016-02-25 16:27:57 +0800

    添加方案3

---
+++ 2-SetNavigationBarBackgroundColorAlpha/1-SetNavigationBarAlpha/UIColor+Extention.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+Extention.h
+//  2-SetNavigationBarBackgroundColorAlpha
+//
+//  Created by 奉强 on 16/2/25.
+//  Copyright © 2016年 奉强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Extention)
+
+
+
+@end
---
+++ 2-SetNavigationBarBackgroundColorAlpha/1-SetNavigationBarAlpha/ViewController.m
@@ -0,0 +1,67 @@
+//
+//  ViewController.m
+//  1-SetNavigationBarAlpha
+//
+//  Created by 奉强 on 16/2/25.
+//  Copyright © 2016年 奉强. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "UIImage+Extention.h"
+#import "UIColor+Extention.h"
+
+#define WIDTH [UIScreen mainScreen].bounds.size.width
+#define HEIGHT [UIScreen mainScreen].bounds.size.height
+
+@interface ViewController () <UIScrollViewDelegate>
+
+@property (nonatomic, strong) UIScrollView *scrollView;
+
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self setScrollView];
+    
+    //设置导航栏背景颜色
+    UIImage *navigationBarBackgroundImage = [UIImage imageFromColor:[UIColor redColor] imageSize:CGSizeMake(WIDTH, 64)];
+    navigationBarBackgroundImage = [UIImage imageByApplyingAlpha:0 image:navigationBarBackgroundImage];
+    [self.navigationController.navigationBar setBackgroundImage:navigationBarBackgroundImage forBarMetrics:UIBarMetricsDefault];
+    
+    self.automaticallyAdjustsScrollViewInsets = NO;
+}
+
+- (void)setScrollView {
+    //设置滚动视图
+    self.scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    UIImage *image = [UIImage imageNamed:@"111.png"];
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, WIDTH, (image.size.width/WIDTH)*HEIGHT)];
+    self.scrollView.contentSize = CGSizeMake(0, imageView.bounds.size.height);
+    self.scrollView.delegate = self;
+    imageView.image = image;
+    
+    [self.scrollView addSubview:imageView];
+    
+    
+    [self.view addSubview:self.scrollView];
+}
+
+#pragma mark scrollViewDelegate
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    //设置导航栏背景透明度
+    CGFloat alpha = (scrollView.contentOffset.y / 200.0f);
+    alpha = alpha < 0 ? 0 : alpha > 1 ? 1 : alpha;
+    
+    UIImage *navigationBarBackgroundImage = [UIImage imageFromColor:[UIColor redColor] imageSize:CGSizeMake(WIDTH, 64)];
+    navigationBarBackgroundImage = [UIImage imageByApplyingAlpha:alpha image:navigationBarBackgroundImage];
+    [self.navigationController.navigationBar setBackgroundImage:navigationBarBackgroundImage forBarMetrics:UIBarMetricsDefault];
+}
+
+
+
+@end
